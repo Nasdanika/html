34661a9a4de90897e87d288e439460fa5df864b1
Pavel <Pavel.Vlasov@nasdanika.org>
2017-02-21 21:59:26 -0500

    Status.toStyle()

--- org.nasdanika.html/src/org/nasdanika/html/FormGroup.java
+++ org.nasdanika.html/src/org/nasdanika/html/FormGroup.java
@@ -2,7 +2,25 @@ package org.nasdanika.html;
 
 public interface FormGroup<T extends FormGroup<T>> extends UIElement<T> {
 	
-	enum Status { SUCCESS, WARNING, ERROR }
+	enum Status { 
+		SUCCESS(Bootstrap.Style.SUCCESS), 
+		WARNING(Bootstrap.Style.WARNING), 
+		ERROR(Bootstrap.Style.DANGER);
+		
+		private Bootstrap.Style style;
+		
+		Status(Bootstrap.Style style) {
+			this.style = style;
+		}
+		
+		/**
+		 * @return Style corresponding to status.
+		 */
+		Bootstrap.Style toStyle() {
+			return style;
+		}
+		
+	}
 
 	T status(Status status);
 	
