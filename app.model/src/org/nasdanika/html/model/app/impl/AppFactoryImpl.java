5734b71a61bae3ae8b11cc1d7eb921edadd92088
Pavel <Pavel.Vlasov@nasdanika.org>
2019-01-28 21:01:56 -0500

    Short folder names, pomless build.

---
+++ app.model/src/org/nasdanika/html/model/app/impl/AppFactoryImpl.java
@@ -0,0 +1,273 @@
+/**
+ */
+package org.nasdanika.html.model.app.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.nasdanika.html.bootstrap.Color;
+import org.nasdanika.html.bootstrap.Theme;
+import org.nasdanika.html.model.app.*;
+import org.nasdanika.html.model.app.Action;
+import org.nasdanika.html.model.app.AppFactory;
+import org.nasdanika.html.model.app.AppPackage;
+import org.nasdanika.html.model.app.ContentAction;
+import org.nasdanika.html.model.app.ContentType;
+import org.nasdanika.html.model.app.Label;
+import org.nasdanika.html.model.app.NavigationActionActivator;
+import org.nasdanika.html.model.app.ScriptActionActivator;
+import org.nasdanika.html.model.app.ThemedAction;
+import org.nasdanika.html.model.app.ThemedContentAction;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class AppFactoryImpl extends EFactoryImpl implements AppFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static AppFactory init() {
+		try {
+			AppFactory theAppFactory = (AppFactory)EPackage.Registry.INSTANCE.getEFactory(AppPackage.eNS_URI);
+			if (theAppFactory != null) {
+				return theAppFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new AppFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AppFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case AppPackage.LABEL: return (EObject)createLabel();
+			case AppPackage.ACTION: return (EObject)createAction();
+			case AppPackage.THEMED_ACTION: return (EObject)createThemedAction();
+			case AppPackage.CONTENT_ACTION: return (EObject)createContentAction();
+			case AppPackage.THEMED_CONTENT_ACTION: return (EObject)createThemedContentAction();
+			case AppPackage.NAVIGATION_ACTION_ACTIVATOR: return (EObject)createNavigationActionActivator();
+			case AppPackage.SCRIPT_ACTION_ACTIVATOR: return (EObject)createScriptActionActivator();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object createFromString(EDataType eDataType, String initialValue) {
+		switch (eDataType.getClassifierID()) {
+			case AppPackage.CONTENT_TYPE:
+				return createContentTypeFromString(eDataType, initialValue);
+			case AppPackage.COLOR:
+				return createColorFromString(eDataType, initialValue);
+			case AppPackage.THEME:
+				return createThemeFromString(eDataType, initialValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String convertToString(EDataType eDataType, Object instanceValue) {
+		switch (eDataType.getClassifierID()) {
+			case AppPackage.CONTENT_TYPE:
+				return convertContentTypeToString(eDataType, instanceValue);
+			case AppPackage.COLOR:
+				return convertColorToString(eDataType, instanceValue);
+			case AppPackage.THEME:
+				return convertThemeToString(eDataType, instanceValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Label createLabel() {
+		LabelImpl label = new LabelImpl();
+		return label;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Action createAction() {
+		ActionImpl action = new ActionImpl();
+		return action;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ThemedAction createThemedAction() {
+		ThemedActionImpl themedAction = new ThemedActionImpl();
+		return themedAction;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ContentAction createContentAction() {
+		ContentActionImpl contentAction = new ContentActionImpl();
+		return contentAction;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ThemedContentAction createThemedContentAction() {
+		ThemedContentActionImpl themedContentAction = new ThemedContentActionImpl();
+		return themedContentAction;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NavigationActionActivator createNavigationActionActivator() {
+		NavigationActionActivatorImpl navigationActionActivator = new NavigationActionActivatorImpl();
+		return navigationActionActivator;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ScriptActionActivator createScriptActionActivator() {
+		ScriptActionActivatorImpl scriptActionActivator = new ScriptActionActivatorImpl();
+		return scriptActionActivator;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ContentType createContentTypeFromString(EDataType eDataType, String initialValue) {
+		ContentType result = ContentType.get(initialValue);
+		if (result == null) throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'");
+		return result;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertContentTypeToString(EDataType eDataType, Object instanceValue) {
+		return instanceValue == null ? null : instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Color createColorFromString(EDataType eDataType, String initialValue) {
+		return (Color)super.createFromString(eDataType, initialValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertColorToString(EDataType eDataType, Object instanceValue) {
+		return super.convertToString(eDataType, instanceValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Theme createThemeFromString(EDataType eDataType, String initialValue) {
+		return (Theme)super.createFromString(eDataType, initialValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertThemeToString(EDataType eDataType, Object instanceValue) {
+		return super.convertToString(eDataType, instanceValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public AppPackage getAppPackage() {
+		return (AppPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static AppPackage getPackage() {
+		return AppPackage.eINSTANCE;
+	}
+
+} //AppFactoryImpl
