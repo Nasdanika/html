495694054bc8decc7b2d8d0db0f6279ea7127574
Pavel <Pavel.Vlasov@nasdanika.org>
2019-02-18 12:57:18 -0500

    Improvements.

--- tests/src/org/nasdanika/html/tests/TestHtml.java
+++ tests/src/org/nasdanika/html/tests/TestHtml.java
@@ -1,7 +1,9 @@
 package org.nasdanika.html.tests;
 
 import org.junit.Test;
+import org.nasdanika.html.Event;
 import org.nasdanika.html.HTMLFactory;
+import org.nasdanika.html.Tag;
 import org.nasdanika.html.impl.HTMLElementImpl;
 
 
@@ -15,6 +17,14 @@ public class TestHtml extends HTMLTestBase {
 	@Test
 	public void testInterpolate() {
 		System.out.println(HTMLFactory.INSTANCE.interpolate(getClass().getResource("test-resource.txt"), "addressee", "world"));		
+	}
+	
+	@Test public void testDiv() {
+		HTMLFactory htmlFactory = HTMLFactory.INSTANCE;
+		Tag div = htmlFactory.div("Hello")
+				.style().border("solid 1px")
+				.on(Event.click, getClass().getResource("ClickHandler.js"));
+		System.out.println(div);
 	}
 		
 }
