fca27a3fc53d098b30007d5434c6b957ef49d84b
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-04-12 00:42:31 -0400

    password hash method.

--- cdo.http/src/org/nasdanika/cdo/http/Util.java
+++ cdo.http/src/org/nasdanika/cdo/http/Util.java
@@ -1,5 +1,9 @@
 package org.nasdanika.cdo.http;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public final class Util {
 	
 	public static final String JSON_CONTENT_TYPE = "application/json";
@@ -15,6 +19,24 @@ public final class Util {
 		return str == null || str.trim().isEmpty();
 	}
 	
+	private static final String DIGEST_ALGORITHM = "SHA-256";
+	private static final String UTF_8 = "UTF-8";	
 	
+	/**
+	 * Computes SHA-256 hash for a password with an optional seed.
+	 * @param seed Seed, can be null.
+	 * @param password Password.
+	 * @return Password hash.
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static byte[] passwordHash(byte[] seed, String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest md = MessageDigest.getInstance(DIGEST_ALGORITHM);
+		if (seed != null) {
+			md.update(seed);
+		}
+		md.update(password.getBytes(UTF_8));
+		return md.digest();
+	}
 
 }
