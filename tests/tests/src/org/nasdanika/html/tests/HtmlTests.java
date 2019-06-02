5ad98a2a03f9362c2f065f44098e8c45c6e9b3a5
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-06-02 14:33:30 -0400

    Tests are executed after the main build.

---
+++ tests/tests/src/org/nasdanika/html/tests/HtmlTests.java
@@ -0,0 +1,17 @@
+package org.nasdanika.html.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TestHtml.class,
+	TestApp.class,
+	TestBootstrap.class,
+	TestFontAwesome.class,	
+//	TestEmf.class,
+})
+public class HtmlTests {
+	
+}
