1c20224f6e842da13b76df32f260cad0d33ccd10
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-07-03 08:21:46 -0400

    Generation of progress reports.

---
+++ app/src/org/nasdanika/html/app/impl/ProgressEntryViewAction.java
@@ -0,0 +1,119 @@
+package org.nasdanika.html.app.impl;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.nasdanika.common.ProgressEntry;
+import org.nasdanika.common.ProgressEntry.Step;
+import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.html.Fragment;
+import org.nasdanika.html.HTMLFactory;
+import org.nasdanika.html.TagName;
+import org.nasdanika.html.app.NavigationActionActivator;
+import org.nasdanika.html.app.ViewGenerator;
+import org.nasdanika.html.bootstrap.BootstrapFactory;
+import org.nasdanika.html.bootstrap.Color;
+import org.nasdanika.html.bootstrap.RowContainer.Row;
+import org.nasdanika.html.bootstrap.Table;
+
+/**
+ * This class can be used to generate HTML reports from progress entries, e.g. a code generation report. 
+ * @author Pavel
+ *
+ */
+public class ProgressEntryViewAction extends ActionImpl {
+	
+	private static AtomicLong counter = new AtomicLong();
+
+	private ProgressEntry progressEntry;
+
+
+	public ProgressEntryViewAction(ProgressEntry progressEntry) {
+		this.progressEntry = progressEntry;
+		setText(progressEntry.getName());
+		
+		for (ProgressEntry peChild: progressEntry.getChildren()) {
+			ProgressEntryViewAction child = new ProgressEntryViewAction(peChild);
+			child.setParent(this);
+			getChildren().add(child);
+		}
+		
+		setId(Long.toString(counter.incrementAndGet(), Character.MAX_RADIX));
+		
+		setActivator(new NavigationActionActivator() {
+			
+			@Override
+			public String getUrl() {
+				return ProgressEntryViewAction.this.getId()+".html";
+			}
+			
+		});
+		
+		getRoles().add(Role.NAVIGATION);
+		
+	}
+		
+	@Override
+	public Object generate(ViewGenerator viewGenerator, ProgressMonitor progressMonitor) {
+		BootstrapFactory bootstrapFactory = viewGenerator.get(BootstrapFactory.class);
+		Table infoTable = bootstrapFactory.table();
+		infoTable.toHTMLElement().style().width("auto");
+		if (progressEntry.getStart() > 0) {
+			Row startRow = infoTable.row();
+			startRow.header("Start");
+			startRow.cell(new Date(progressEntry.getStart()));
+		}
+		if (progressEntry.getFinish() > progressEntry.getStart()) {
+			Row durationRow = infoTable.row();
+			durationRow.header("Duration");
+			durationRow.cell(progressEntry.getFinish() - progressEntry.getStart());
+		}
+		if (progressEntry.getTotalWork() > 0) {
+			Row totalWorkRow = infoTable.row();
+			totalWorkRow.header("Total work");
+			totalWorkRow.cell(progressEntry.getTotalWork());			
+		}
+		
+		HTMLFactory htmlFactory = bootstrapFactory.getHTMLFactory();
+		Fragment fragment = htmlFactory.fragment(infoTable);
+		
+		if (progressEntry.getDetails().length > 0) {
+			fragment.content(TagName.h2.create("Details"));
+			Table detailsTable = bootstrapFactory.table().bordered();
+			detailsTable.headerRow("Type", "Value");
+			for (Object obj: progressEntry.getDetails()) {
+				detailsTable.row(obj != null ? obj.getClass().getName() : "", String.valueOf(obj));
+			}
+			fragment.content(detailsTable);
+		}
+		
+		if (!progressEntry.getSteps().isEmpty()) {
+			fragment.content(TagName.h2.create("Steps"));
+			Table stepsTable = bootstrapFactory.table().bordered();
+			stepsTable.headerRow("Time", "Status", "Worked", "Message");
+			for (Step step: progressEntry.getSteps()) {				
+				Row stepRow = stepsTable.row(new Date(step.getTime()), step.getStatus().name(), step.getWorked(), step.getMessage());
+				switch (step.getStatus()) {
+				case CANCEL:
+					stepRow.color(Color.MUTED);
+					break;
+				case WARNING:
+					stepRow.color(Color.WARNING);
+					break;
+				case ERROR:
+					stepRow.color(Color.DANGER);
+					break;
+				case SUCCESS:
+					stepRow.color(Color.SUCCESS); 						
+					break;
+				default:
+					break; // INFO.
+				}
+			}
+			fragment.content(stepsTable);			
+		}
+		
+		return fragment;
+	}
+
+}
---
+++ app/src/org/nasdanika/html/app/impl/ProgressReportGenerator.java
@@ -0,0 +1,79 @@
+package org.nasdanika.html.app.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.nasdanika.common.ProgressEntry;
+import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.html.app.Action;
+import org.nasdanika.html.app.Application;
+import org.nasdanika.html.app.ApplicationBuilder;
+import org.nasdanika.html.bootstrap.Theme;
+import org.nasdanika.html.fontawesome.FontAwesomeFactory;
+import org.nasdanika.html.jstree.JsTreeFactory;
+
+/**
+ * Generates HTML progress report from {@link ProgressEntry}
+ * @author Pavel
+ *
+ */
+public class ProgressReportGenerator {
+
+	private ActionImpl principalAction;
+	private ActionImpl rootAction;
+
+	public ProgressReportGenerator(String title, ProgressEntry... progressEntries) {
+		this(title, Arrays.asList(progressEntries));
+	}	
+	
+	/**
+	 * Generates progress report
+	 * @param title
+	 * @param description
+	 */
+	public ProgressReportGenerator(String title, List<ProgressEntry> progressEntries) {
+		rootAction = new ActionImpl();
+		rootAction.setText(title);
+		
+		principalAction = new ActionImpl();
+		principalAction.setParent(rootAction);
+		rootAction.getChildren().add(principalAction);
+
+		boolean isFirst = true;
+		for (ProgressEntry pe: progressEntries) {
+			ProgressEntryViewAction peva = new ProgressEntryViewAction(pe);
+			principalAction.getChildren().add(peva);
+			peva.setParent(principalAction);
+			if (isFirst) {
+				peva.setId("index");
+				isFirst = false;
+			}
+		}
+	}
+	
+	public void generate(org.nasdanika.common.resources.Container<Object> resourceConsumer, ProgressMonitor progressMonitor) {
+		for (Action action: principalAction.getChildren()) {
+			generate(action, resourceConsumer, progressMonitor);
+		}
+	}
+
+	protected void generate(Action action, org.nasdanika.common.resources.Container<Object> resourceConsumer, ProgressMonitor progressMonitor) {		
+		try (ProgressMonitor im = progressMonitor.split("Generating report for "+action.getText(), 100)) {
+			Application app = new BootstrapContainerRouterApplication(Theme.Default, true);
+
+			JsTreeFactory.INSTANCE.cdn(app.getHTMLPage());
+			FontAwesomeFactory.INSTANCE.cdn(app.getHTMLPage());
+			
+			ApplicationBuilder  applicationBuilder = new ActionApplicationBuilder(action);
+			
+			applicationBuilder.build(app, progressMonitor);
+			
+			resourceConsumer.getFile(action.getId()+".html").setContents(app, im);			
+		}
+		for (Action child: action.getChildren()) {
+			generate(child, resourceConsumer, progressMonitor);
+		}		
+	}
+			
+}
+
