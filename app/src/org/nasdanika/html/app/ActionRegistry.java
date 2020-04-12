c5fd9ea4c44f2b14fb470002d5161f4eee36a2b8
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-04-12 08:32:28 -0400

    As property computer implementation.

--- app/src/org/nasdanika/html/app/ActionRegistry.java
+++ app/src/org/nasdanika/html/app/ActionRegistry.java
@@ -29,32 +29,33 @@ public interface ActionRegistry {
 			
 			@SuppressWarnings("unchecked")
 			@Override
-			public <T> T compute(Context context, String key, Class<T> type) {
-				return (T) ("TODO " + key);
-//				Action action = ActionRegistry.this.get(key);
-//				if (action == null) {
-//					return null;
-//				}
-//				
-//				if (subKey == null) {
-//					Tag link = context.get(ViewGenerator.class, new ViewGeneratorImpl(context, null, null)).link(action);
-//					return (T) (type.isAssignableFrom(Tag.class) ? link : link.toString());
-//				}
-//				
-//				switch (subKey) {
-//				case "text":
-//					return (T) action.getText();
-//				case "icon":
-//					return (T) action.getIcon();
-//				case "url":
-//					ActionActivator activator = action.getActivator();
-//					if (activator instanceof NavigationActionActivator) {
-//						return (T) ((NavigationActionActivator) activator).getUrl(context.getString(Context.BASE_URI_PROPERTY));
-//					}
-//					return null;
-//				default:
-//					return null;
-//				}
+			public <T> T compute(Context context, String key, String path, Class<T> type) {
+				if (path != null) {
+					int slashIdx = path.indexOf('/');
+					Action action = ActionRegistry.this.get(slashIdx == -1 ? path : path.substring(0, slashIdx));
+					if (action != null) {
+						if (slashIdx == -1) {
+							Tag link = context.get(ViewGenerator.class, new ViewGeneratorImpl(context, null, null)).link(action);
+							return (T) (type.isAssignableFrom(Tag.class) ? link : link.toString());
+						}
+						
+						switch (path.substring(slashIdx + 1)) {
+						case "text":
+							return (T) action.getText();
+						case "icon":
+							return (T) action.getIcon();
+						case "url":
+							ActionActivator activator = action.getActivator();
+							if (activator instanceof NavigationActionActivator) {
+								return (T) ((NavigationActionActivator) activator).getUrl(context.getString(Context.BASE_URI_PROPERTY));
+							}
+							return null;
+						default:
+							return null;
+						}
+					}
+				}
+				return null;
 			}
 		};
 	}
