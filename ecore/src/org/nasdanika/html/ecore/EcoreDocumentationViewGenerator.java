5056fb65ae9ef57237ac2213c0b035d2b98561c8
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-04-26 09:03:53 -0400

    Fix.

--- ecore/src/org/nasdanika/html/ecore/EcoreDocumentationViewGenerator.java
+++ ecore/src/org/nasdanika/html/ecore/EcoreDocumentationViewGenerator.java
@@ -1,5 +1,6 @@
 package org.nasdanika.html.ecore;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 import org.eclipse.emf.ecore.EClass;
@@ -15,40 +16,42 @@ import org.nasdanika.html.jstree.JsTreeNode;
 
 /**
  * Customized {@link ViewGenerator} for rendering Ecore documentation.
+ * 
  * @author Pavel
  *
  */
 public class EcoreDocumentationViewGenerator extends ViewGeneratorImpl {
-		
-		public EcoreDocumentationViewGenerator(Context context, Consumer<?> headContentConsumer, Consumer<?> bodyContentConsumer) {
+
+	public EcoreDocumentationViewGenerator(Context context, Consumer<?> headContentConsumer,
+			Consumer<?> bodyContentConsumer) {
 		super(context, headContentConsumer, bodyContentConsumer);
 	}
 
-		/**
-		 * Special handling of Eclasses - abstract are italic, interfaces are in dark
-		 */
-		@Override
-		public void label(Label label, Consumer<Object> contentConsumer) {
-			if (label instanceof Action) {
-				 EClass eClass = ((Action) label).adaptTo(EClass.class);
-				 if (eClass != null && eClass.isAbstract()) {
-					 Tag span = get(HTMLFactory.class).span();
-					 get(BootstrapFactory.class).wrap(span).text().italic();
-					 contentConsumer.accept(span);
-					 contentConsumer = span;
-				 }
+	/**
+	 * Special handling of Eclasses - abstract are italic, interfaces are in dark
+	 */
+	@Override
+	public void label(Label label, Consumer<Object> contentConsumer) {
+		if (label instanceof Action) {
+			EClass eClass = ((Action) label).adaptTo(EClass.class);
+			if (eClass != null && eClass.isAbstract()) {
+				Tag span = get(HTMLFactory.class).span();
+				get(BootstrapFactory.class).wrap(span).text().italic();
+				contentConsumer.accept(span);
+				contentConsumer = span;
 			}
-			super.label(label, contentConsumer);
 		}
-		
-		@Override
-		public JsTreeNode jsTreeNode(Action action, boolean ajax) {
-			JsTreeNode ret = super.jsTreeNode(action, ajax);
-			 EClass eClass = action.adaptTo(EClass.class);
-			 if (eClass != null && eClass.isAbstract()) {
-				 ret.anchorAttribute("style", "font-style:italic");
-			 }
-			return ret;
+		super.label(label, contentConsumer);
+	}
+
+	@Override
+	public JsTreeNode jsTreeNode(Action action, boolean ajax, BiFunction<Action, JsTreeNode, JsTreeNode> filter) {
+		JsTreeNode ret = super.jsTreeNode(action, ajax, filter);
+		EClass eClass = action.adaptTo(EClass.class);
+		if (ret != null && eClass != null && eClass.isAbstract()) {
+			ret.anchorAttribute("style", "font-style:italic");
 		}
-		
-	};
+		return ret;
+	}
+
+};
