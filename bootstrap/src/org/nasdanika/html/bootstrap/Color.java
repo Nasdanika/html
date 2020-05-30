c9e1b0bd42d9ee4274972bb2c3d2a635667b28ec
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-05-30 14:17:21 -0400

    Applicability, body color.

--- bootstrap/src/org/nasdanika/html/bootstrap/Color.java
+++ bootstrap/src/org/nasdanika/html/bootstrap/Color.java
@@ -1,12 +1,17 @@
 package org.nasdanika.html.bootstrap;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Bootstrap colors. Not all colors are applicable in all situations. E.g. TRANSPARENT is applicable for backgrounds.
  * @author Pavel Vlasov
  *
  */
 public enum Color {
-	
+			
 	/**
 	 * No color - for borders means that there should be no border. 
 	 * For other uses is the same as DEFAULT
@@ -24,19 +29,54 @@ public enum Color {
 	INFO("info", "Info"),
 	LIGHT("light", "Light"),
 	DARK("dark", "Dark"),
-	MUTED("muted", "Muted"),
+	BODY("body", "Body", Applicability.TEXT),
+	MUTED("muted", "Muted", Applicability.TEXT),
 	WHITE("white", "White"),
-	BLACK50("black-50", "Black 50%"),
-	WHITE50("white-50", "White 50%"),
-	TRANSPARENT("transparent", "Transparent");
+	BLACK50("black-50", "Black 50%", Applicability.TEXT),
+	WHITE50("white-50", "White 50%", Applicability.TEXT),
+	TRANSPARENT("transparent", "Transparent", Applicability.BACKGROUND);
 	
-	Color(String code, String label) {
+	/**
+	 * Defines where the color can be applied.
+	 * @author Pavel
+	 *
+	 */
+	public static enum Applicability {
+		BACKGROUND,
+		BORDER,
+		TEXT;
+		
+		/**
+		 * @return Applicable colors;
+		 */
+		public List<Color> colors() {
+			return Arrays.stream(Color.values()).filter(c -> c.isApplicable(this)).collect(Collectors.toList());
+		}
+		
+		/**
+		 * @return Labels of applicable colors with suppressed duplicates - only one empty string for both NONE and DEFAULT.
+		 */
+		public List<Object> colorLabels() {
+			List<Object> ret = new ArrayList<>();
+			for (Color c: colors()) {
+				if (!ret.contains(c.label)) {
+					ret.add(c.label);
+				}
+			}		
+			return ret;
+		}
+		
+	}
+	
+	Color(String code, String label, Applicability... applicability) {
 		this.code = code;
 		this.label = label;
+		this.applicabilities = applicability;
 	}
 	
 	public final String code;
 	public final String label;
+	private final Applicability[] applicabilities; 
 		
 	public static Color fromLabel(String label) {
 		for (Color color: values()) {
@@ -47,5 +87,16 @@ public enum Color {
 		throw new IllegalArgumentException("No color value for label "+label);
 	}
 	
+	public boolean isApplicable(Applicability applicability) {
+		if (applicabilities.length == 0) {
+			return true;
+		}
+		for (Applicability a: applicabilities) {
+			if (a == applicability) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
