6557e27f465113752d3cd7303fced49c7653beef
Pavel <Pavel.Vlasov@nasdanika.org>
2020-07-21 00:20:09 -0400

    Support of alternative text in action registry.

--- app/src/org/nasdanika/html/app/ActionRegistry.java
+++ app/src/org/nasdanika/html/app/ActionRegistry.java
@@ -5,6 +5,7 @@ import java.util.function.BiFunction;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.PropertyComputer;
 import org.nasdanika.html.Tag;
+import org.nasdanika.html.app.impl.ActionFilter;
 import org.nasdanika.html.app.impl.ViewGeneratorImpl;
 
 /**
@@ -31,11 +32,25 @@ public interface ActionRegistry {
 			@Override
 			public <T> T compute(Context context, String key, String path, Class<T> type) {
 				if (path != null) {
+					int spaceIdx = path.indexOf(' ');
+					String text = spaceIdx == -1 ? null : path.substring(spaceIdx + 1);
+					if (spaceIdx != -1) {
+						path = path.substring(0, spaceIdx);
+					}
 					int slashIdx = path.indexOf('/');
 					Action action = ActionRegistry.this.get(slashIdx == -1 ? path : path.substring(0, slashIdx));
 					if (action != null) {
 						if (slashIdx == -1) {
-							Tag link = context.get(ViewGenerator.class, new ViewGeneratorImpl(context, null, null)).link(action);
+							Action actionFilter = new ActionFilter<Action>(action) {
+								
+								@Override
+								public String getText() {									
+									return text == null ? super.getText() : text;
+								}
+								
+							};
+							
+							Tag link = context.get(ViewGenerator.class, new ViewGeneratorImpl(context, null, null)).link(actionFilter);
 							return (T) (type.isAssignableFrom(Tag.class) ? link : link.toString());
 						}
 						
