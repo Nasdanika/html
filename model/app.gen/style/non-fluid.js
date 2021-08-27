cca1931f9fa4727c96e63ad603a4e8d85fa17b2a
Pavel <Pavel.Vlasov@nasdanika.org>
2021-08-27 08:13:58 -0400

    Styling of page elements.

---
+++ model/app.gen/style/non-fluid.js
@@ -0,0 +1,4 @@
+// Border for a non-fluid container and top margin
+$(document).ready( function() {
+    $('.nsd-app-container').addClass(['border', 'border-dark', 'mt-1']);
+});
---
+++ model/app.gen/style/primary-head.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+    $('.nsd-app-header').addClass(['bg-primary', 'text-secondary']);
+    $('.nsd-app-header .nav-link').addClass('text-secondary');
+    $('.nsd-app-header-title').addClass(['display-4', 'text-secondary']);
+
+    $('.nsd-app-navbar').addClass(['bg-secondary', 'p-0']);
+
+    $('.nsd-app-navigation-panel').removeClass('col'); 
+    $('.nsd-app-navigation-panel').addClass(['col-auto', 'border-right', 'border-secondary', 'pb-1']); 
+    $('.nsd-app-content-panel').addClass('p-1');
+    $('.nsd-app-footer').addClass(['bg-secondary', 'text-primary', 'text-center']);
+    $('.nsd-app-footer .nsd-footer-action').addClass('font-italic');
+});
