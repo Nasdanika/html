0c2df67e6f1d603b9c5dc15bf0cd0fc13363acf2
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2021-10-12 18:39:53 +0100

    Table sections.

--- model/bootstrap.gen/src/main/java/org/nasdanika/html/model/bootstrap/gen/TableRowContainerConsumerFactoryAdapter.java
+++ model/bootstrap.gen/src/main/java/org/nasdanika/html/model/bootstrap/gen/TableRowContainerConsumerFactoryAdapter.java
@@ -1,11 +1,33 @@
 package org.nasdanika.html.model.bootstrap.gen;
 
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.nasdanika.common.CollectionCompoundConsumerFactory;
+import org.nasdanika.common.ConsumerFactory;
+import org.nasdanika.common.Context;
+import org.nasdanika.common.Function;
+import org.nasdanika.emf.EObjectAdaptable;
+import org.nasdanika.html.HTMLElement;
+import org.nasdanika.html.model.bootstrap.TableRow;
+
 public class TableRowContainerConsumerFactoryAdapter<M extends org.nasdanika.html.model.bootstrap.TableRowContainer, T extends org.nasdanika.html.bootstrap.RowContainer<?,?>> extends BootstrapElementConsumerFactoryAdapter<M,T> {
 
 	public TableRowContainerConsumerFactoryAdapter(M tableRowContainer) {
 		super(tableRowContainer);
 	}
 	
-	// TODO - rows
+	@Override
+	protected Function<HTMLElement<?>, HTMLElement<?>> createConfigureFunction(Context context) throws Exception {
+		EList<TableRow> rows = getTarget().getRows();
+		if (rows.isEmpty()) {
+			return super.createConfigureFunction(context);
+		}
+	
+		@SuppressWarnings("rawtypes")
+		List<ConsumerFactory<HTMLElement>> rowConsumers = EObjectAdaptable.adaptToConsumerFactoryNonNull(rows, org.nasdanika.html.HTMLElement.class);
+		CollectionCompoundConsumerFactory<HTMLElement<?>> rowsConsumerFactory = new CollectionCompoundConsumerFactory<>("Creating rows", rowConsumers);			
+		return super.createConfigureFunction(context).then(rowsConsumerFactory.asFunctionFactory().create(context));
+	}
 
 }
