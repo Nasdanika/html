79b8c9eaa680b3caf59605762bcc513aa84b7529
Pavel <Pavel.Vlasov@nasdanika.org>
2021-10-17 09:23:51 -0400

    Transitions and calls tables.

--- flow/src/main/java/org/nasdanika/html/flow/ArtifactActionProvider.java
+++ flow/src/main/java/org/nasdanika/html/flow/ArtifactActionProvider.java
@@ -1,11 +1,22 @@
 package org.nasdanika.html.flow;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.ETypedElement;
 import org.nasdanika.common.Context;
+import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.flow.Artifact;
 import org.nasdanika.flow.FlowPackage;
+import org.nasdanika.flow.Transition;
+import org.nasdanika.html.emf.ColumnBuilder;
+import org.nasdanika.html.model.app.Action;
+import org.nasdanika.html.model.bootstrap.TableCell;
 
 public class ArtifactActionProvider extends PackageElementActionProvider<Artifact> {
 	
@@ -18,9 +29,183 @@ public class ArtifactActionProvider extends PackageElementActionProvider<Artifac
 		List<ETypedElement> properties = super.getProperties();
 		properties.add(FlowPackage.Literals.ARTIFACT__INPUT_FOR);
 		properties.add(FlowPackage.Literals.ARTIFACT__OUTPUT_FOR);
-		properties.add(FlowPackage.Literals.ARTIFACT__PAYLOAD_FOR);
 		properties.add(FlowPackage.Literals.ARTIFACT__REPOSITORIES);
 		return properties;
+	}
+		
+	@Override
+	protected void resolve(
+			Action action, 
+			org.nasdanika.html.emf.EObjectActionResolver.Context context,
+			ProgressMonitor progressMonitor) throws Exception {
+		
+		super.resolve(action, context, progressMonitor);
+	
+		EList<Action> sections = action.getSections();
+		if (!getTarget().getPayloadFor().isEmpty()) {
+			sections.add(createEReferenceAction(action, FlowPackage.Literals.ARTIFACT__PAYLOAD_FOR, context, progressMonitor));
+		}
+		
+		if (!getTarget().getResponseFor().isEmpty()) {
+			sections.add(createEReferenceAction(action, FlowPackage.Literals.ARTIFACT__RESPONSE_FOR, context, progressMonitor));
+		}
+	}
+	
+	// TODO - Sort by source 
+	private Action createEReferenceAction(
+			Action action, 
+			EReference eReference,
+			org.nasdanika.html.emf.EObjectActionResolver.Context context,
+			ProgressMonitor progressMonitor) throws Exception {
+		Collection<ColumnBuilder<EObject>> columnBuilders = new ArrayList<>();
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Source"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				cell.getContent().add(renderValue(base, typedElement, rowElement.eContainer().eContainer(), context, progressMonitor));				
+			}
+		});
+		
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Key"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				EObject renderedValue = renderValue(base, typedElement, ((Map.Entry<?,?>) rowElement.eContainer()).getKey(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+		
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Target"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				EObject renderedValue = renderValue(base, typedElement, ((Transition) rowElement).getTarget(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Name"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				EObject renderedValue = renderValue(base, typedElement, ((Transition) rowElement).getName(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Documentation"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				EObject renderedValue = renderValue(base, typedElement, ((Transition) rowElement).getDocumentation(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+		
+		return createTableAction(
+				eReference,
+				columnBuilders,
+				action, 
+				context, 
+				progressMonitor);
 	}
 	
 }
--- flow/src/main/java/org/nasdanika/html/flow/FlowElementActionProvider.java
+++ flow/src/main/java/org/nasdanika/html/flow/FlowElementActionProvider.java
@@ -10,6 +10,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.ETypedElement;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.flow.Call;
 import org.nasdanika.flow.FlowElement;
 import org.nasdanika.flow.FlowPackage;
 import org.nasdanika.flow.Transition;
@@ -46,14 +47,24 @@ public class FlowElementActionProvider<T extends FlowElement<?>> extends Package
 	
 		// Inputs
 		EList<Action> sections = action.getSections();
-		sections.add(createInputsAction(action, context, progressMonitor));
+		if (!getTarget().getInputs().isEmpty()) {
+			sections.add(createInputsAction(action, context, progressMonitor));
+		}
 		
 		// Outputs
-		sections.add(createOutputsAction(action, context, progressMonitor));
+		if (!getTarget().getOutputs().isEmpty()) {
+			sections.add(createOutputsAction(action, context, progressMonitor));
+		}
 		
 		// Invocations
+		if (!getTarget().getInvocations().isEmpty()) {
+			sections.add(createInvocationsAction(action, context, progressMonitor));
+		}
 		
 		// Calls
+		if (!getTarget().getCalls().isEmpty()) {
+			sections.add(createCallsAction(action, context, progressMonitor));
+		}
 	}
 
 	private Action createInputsAction(
@@ -87,8 +98,8 @@ public class FlowElementActionProvider<T extends FlowElement<?>> extends Package
 			}
 		});
 		
-//		columnBuilders.add(createColumnBuilder(FlowPackage.Literals.TRANSITION__TARGET));		
 		columnBuilders.add(createColumnBuilder(NcorePackage.Literals.NAMED_ELEMENT__NAME));		
+		columnBuilders.add(createColumnBuilder(FlowPackage.Literals.PACKAGE_ELEMENT__DOCUMENTATION));		
 		columnBuilders.add(createColumnBuilder(FlowPackage.Literals.TRANSITION__PAYLOAD));
 		
 		return createTableAction(
@@ -203,6 +214,36 @@ public class FlowElementActionProvider<T extends FlowElement<?>> extends Package
 					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
 					ProgressMonitor progressMonitor)
 					throws Exception {
+				header.getContent().add(createText("Documentation"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				Transition value = (Transition) ((Map.Entry<?,?>) rowElement).getValue();
+				EObject renderedValue = renderValue(base, typedElement, value.getDocumentation(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
 				header.getContent().add(createText("Payload"));
 			}
 			
@@ -225,6 +266,242 @@ public class FlowElementActionProvider<T extends FlowElement<?>> extends Package
 		
 		return createTableAction(
 				FlowPackage.Literals.FLOW_ELEMENT__OUTPUTS,
+				columnBuilders,
+				action, 
+				context, 
+				progressMonitor);
+	}
+
+	private Action createInvocationsAction(
+			Action action, 
+			org.nasdanika.html.emf.EObjectActionResolver.Context context,
+			ProgressMonitor progressMonitor) throws Exception {
+		Collection<ColumnBuilder<EObject>> columnBuilders = new ArrayList<>();
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Source"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				cell.getContent().add(renderValue(base, typedElement, rowElement.eContainer().eContainer(), context, progressMonitor));				
+			}
+		});
+		
+		columnBuilders.add(createColumnBuilder(NcorePackage.Literals.NAMED_ELEMENT__NAME));		
+		columnBuilders.add(createColumnBuilder(FlowPackage.Literals.PACKAGE_ELEMENT__DOCUMENTATION));		
+		columnBuilders.add(createColumnBuilder(FlowPackage.Literals.TRANSITION__PAYLOAD));
+		columnBuilders.add(createColumnBuilder(FlowPackage.Literals.CALL__RESPONSE));
+		
+		return createTableAction(
+				FlowPackage.Literals.FLOW_ELEMENT__INVOCATIONS,
+				columnBuilders,
+				action, 
+				context, 
+				progressMonitor);
+	}
+	
+	private Action createCallsAction(
+			Action action, 
+			org.nasdanika.html.emf.EObjectActionResolver.Context context,
+			ProgressMonitor progressMonitor) throws Exception {
+		Collection<ColumnBuilder<EObject>> columnBuilders = new ArrayList<>();
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Key"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				EObject renderedValue = renderValue(base, typedElement, ((Map.Entry<?,?>) rowElement).getKey(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+		
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Target"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				Transition transition = (Transition) ((Map.Entry<?,?>) rowElement).getValue();
+				EObject renderedValue = renderValue(base, typedElement, transition.getTarget(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Name"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				Transition value = (Transition) ((Map.Entry<?,?>) rowElement).getValue();
+				EObject renderedValue = renderValue(base, typedElement, value.getName(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Documentation"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				Transition value = (Transition) ((Map.Entry<?,?>) rowElement).getValue();
+				EObject renderedValue = renderValue(base, typedElement, value.getDocumentation(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Request"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				Transition value = (Transition) ((Map.Entry<?,?>) rowElement).getValue();
+				EObject renderedValue = renderValue(base, typedElement, value.getPayload(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);				
+				}
+			}
+		});
+
+		columnBuilders.add(new ColumnBuilder<EObject>() {
+			
+			@Override
+			public void buildHeader(
+					TableCell header, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				header.getContent().add(createText("Response"));
+			}
+			
+			@Override
+			public void buildCell(
+					EObject rowElement, 
+					TableCell cell, 
+					Action base, 
+					ETypedElement typedElement,
+					org.nasdanika.html.emf.EObjectActionResolver.Context context, 
+					ProgressMonitor progressMonitor)
+					throws Exception {
+				Call value = (Call) ((Map.Entry<?,?>) rowElement).getValue();
+				EObject renderedValue = renderValue(base, typedElement, value.getResponse(), context, progressMonitor);
+				if (renderedValue != null) {
+					cell.getContent().add(renderedValue);				
+				}
+			}
+		});
+		
+		return createTableAction(
+				FlowPackage.Literals.FLOW_ELEMENT__CALLS,
 				columnBuilders,
 				action, 
 				context, 
