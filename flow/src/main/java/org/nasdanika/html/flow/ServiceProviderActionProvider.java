d07ef8c89ddb3ce80c12939d43ca85226d2428a2
Pavel <Pavel.Vlasov@nasdanika.org>
2021-12-14 08:57:17 -0500

    Services work in progress, copy to clipboard issue details.

--- flow/src/main/java/org/nasdanika/html/flow/ServiceProviderActionProvider.java
+++ flow/src/main/java/org/nasdanika/html/flow/ServiceProviderActionProvider.java
@@ -1,11 +1,21 @@
 package org.nasdanika.html.flow;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
-import org.eclipse.emf.ecore.ETypedElement;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.nasdanika.common.Context;
+import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.flow.Activity;
 import org.nasdanika.flow.FlowPackage;
 import org.nasdanika.flow.ServiceProvider;
+import org.nasdanika.html.model.app.Action;
+import org.nasdanika.html.model.app.AppFactory;
+import org.nasdanika.ncore.util.NamedElementComparator;
 
 public class ServiceProviderActionProvider<T extends ServiceProvider<?>> extends PackageElementActionProvider<T> {
 	
@@ -14,10 +24,47 @@ public class ServiceProviderActionProvider<T extends ServiceProvider<?>> extends
 	}
 	
 	@Override
-	protected List<ETypedElement> getProperties() {
-		List<ETypedElement> properties = super.getProperties();
-		properties.add(FlowPackage.Literals.SERVICE_PROVIDER__SERVICES);
-		return properties;
+	protected Action createAction(
+			BiConsumer<EObject, Action> registry,
+			Consumer<org.nasdanika.common.Consumer<org.nasdanika.html.emf.EObjectActionResolver.Context>> resolveConsumer,
+			ProgressMonitor progressMonitor) throws Exception {
+		
+		Action action = super.createAction(registry, resolveConsumer, progressMonitor);
+		List<Activity<?>> services = getTarget().getServices().values().stream().sorted(NamedElementComparator.INSTANCE).collect(Collectors.toList());
+		if (!services.isEmpty()) {
+			Action group = AppFactory.eINSTANCE.createAction();
+			group.setText("Services");
+//			group.setDescription("Shared activities or journeys which can be used on their own or be referenced from other journeys."); TODO - description modal.
+			group.setTooltip("Shared activities or journeys which can be used on their own or be referenced from other journeys.");
+			group.setUuid(action.getUuid() + "-services");
+			EList<Action> anonymous = action.getAnonymous();
+			for (Activity<?> service: getTarget().getServices().values().stream().sorted(NamedElementComparator.INSTANCE).collect(Collectors.toList())) {
+				anonymous.add(createChildAction(service, registry, resolveConsumer, progressMonitor));
+			}
+		
+			action.getSections().add(group);
+		}
+		
+		return action;
 	}
-
+		
+	@Override
+	protected void resolve(
+			Action action, 
+			org.nasdanika.html.emf.EObjectActionResolver.Context context,
+			ProgressMonitor progressMonitor) throws Exception {
+		super.resolve(action, context, progressMonitor);
+				
+		List<Activity<?>> services = getTarget().getServices().values().stream().sorted(NamedElementComparator.INSTANCE).collect(Collectors.toList());
+		if (!services.isEmpty()) {
+			String servicesGroupUUID = action.getUuid() + "-services";
+			Optional<Action> servicesActionOptional = action.getSections().stream()
+					.filter(a -> servicesGroupUUID.equals(a.getUuid()))
+					.findFirst();
+			
+			Action servicesAction = servicesActionOptional.get();
+			servicesAction.getContent().add(renderList(services, false, null, action, FlowPackage.Literals.SERVICE_PROVIDER__SERVICES, context, progressMonitor));
+		}
+	}
+	
 }
