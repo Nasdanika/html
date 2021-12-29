980d2776206807e9877d7d376b29cb34db98916f
Pavel <Pavel.Vlasov@nasdanika.org>
2021-12-28 20:58:36 -0500

    Stores visible fields configuration.

--- model/app.gen/web-resources/js/components/table.js
+++ model/app.gen/web-resources/js/components/table.js
@@ -8,6 +8,48 @@ Vue.component('nsd-table', {
     	items: Array,
     	configKey: String
   	},
+  	data() {
+		let data = {
+			totalRows: 1,
+        	currentPage: 1,
+        	perPage: 20,
+        	pageOptions: [5, 10, 15, { value: 100, text: "Show a lot" }],
+        	sortBy: '',
+        	sortDesc: false,
+        	sortDirection: 'asc',
+        	filter: null,
+        	filterOn: [],
+        	visibleFields: null,
+        	config: {
+				items: null,
+				columns: ['field', 'visible'],
+				visibleFields: null
+			}        			
+		};
+		
+		if (this.configKey && window.localStorage) {
+			let storedConfigStr = window.localStorage.getItem(this.configKey);
+			if (storedConfigStr) {
+				try {
+					let storedConfig = JSON.parse(storedConfigStr);
+					data.visibleFields = storedConfig.visibleFields;
+				} catch (error) {
+					console.error(error);
+				}
+			}
+			if (!data.visibleFields) {
+				data.visibleFields = [];
+				this.columns.forEach(element => {
+					if (element.visible) {
+						data.visibleFields.push(element.key);
+					}
+				});				
+				
+			}			
+		}
+		
+		return data;
+	},
 	computed: {
 		fields() {
 			return this.columns.filter(this.filterColumns);
@@ -18,24 +60,87 @@ Vue.component('nsd-table', {
 	},
 	methods: {
 		filterColumns(column) {
-			// TODO - from local storage.
-			
-			return column.visible;
+			return this.visibleFields.indexOf(column.key) !== -1;
 		},
 		filterItems(item) {
 			return true;
+		},
+		configModal() {
+			this.config.items = [];
+			this.columns.forEach(element => {
+				this.config.items.push({
+					key: element.key, 
+					field: element.label });
+			});
+			this.config.visibleFields = this.visibleFields.slice();
+			
+			this.$root.$emit('bv::show::modal', 'config-modal');
+		},
+		saveConfig() {
+			this.visibleFields = this.config.visibleFields.slice();			
+			if (this.configKey && window.localStorage) {
+				let storedConfig = {
+					visibleFields: this.visibleFields
+				};
+				window.localStorage.setItem(this.configKey, JSON.stringify(storedConfig));
+			}
 		}
+		
 	}, 
 	template: `
 		<b-container fluid>		
-			<b-table striped hover :items="rows" :fields="fields">
-				<template #head()="data">
-					<span v-html="data.label"></span>
-	      		</template>
-				<template #cell()="data">
-					<span v-html="data.value"></span>
-	      		</template>
-			</b-table>
+			<b-row class="my-1">
+				<b-col cols="10">
+					<b-form-group
+			          label="Filter"
+			          label-for="filter-input"
+			          label-cols-sm="3"
+			          label-align-sm="right"
+			          label-size="sm"
+			          class="mb-0"
+			        >
+			          <b-input-group size="sm">
+			            <b-form-input
+			              id="filter-input"
+			              v-model="filter"
+			              type="search"
+			              placeholder="Type to Search"
+			            ></b-form-input>
+			
+			            <b-input-group-append>
+			              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+			            </b-input-group-append>
+			          </b-input-group>
+			        </b-form-group>				
+				</b-col>
+				<b-col cols="2">
+					<b-button title="Configuration" size="sm" @click="configModal">
+						<i class="fas fa-cog"></i>
+        			</b-button>
+ 					<b-modal id="config-modal" size="lg" scrollable title="Table configuration" @ok="saveConfig">
+						<b-table striped hover :items="config.items" :fields="config.columns">
+							<template #cell(field)="data">
+								<span v-html="data.value"></span>
+				      		</template>
+							<template #cell(visible)="data">
+								<input type="checkbox" :value="data.item.key" v-model="config.visibleFields">
+				      		</template>
+						</b-table>	    					
+  					</b-modal>        			
+				</b-col>
+			</b-row>
+			<b-row>
+				<b-col>
+					<b-table striped hover :items="rows" :fields="fields">
+						<template #head()="data">
+							<span v-html="data.label"></span>
+			      		</template>
+						<template #cell()="data">
+							<span v-html="data.value"></span>
+			      		</template>
+					</b-table>
+				</b-row>
+			</b-col>
 		</b-container>
 	`
 });
