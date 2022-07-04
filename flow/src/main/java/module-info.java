e5a94ac43f6b181c4f3013e196eff351bd157b60
Pavel <Pavel.Vlasov@nasdanika.org>
2022-07-04 14:29:56 -0400

    Commented out PlantUML representations
    Will migrate to Mermaid in the future.

--- flow/src/main/java/module-info.java
+++ flow/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module org.nasdanika.html.flow {
 	requires transitive org.nasdanika.emf;
 	requires transitive org.nasdanika.flow;
 	requires org.nasdanika.diagram.gen;
+	requires org.nasdanika.html.emf;
+	requires org.apache.commons.codec;
 	
 	exports org.nasdanika.html.flow;	
 	
