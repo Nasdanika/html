4f2e89326e5c0fd3e092415c33172bfe4d6f5f3d
Pavel <Pavel.Vlasov@nasdanika.org>
2023-02-18 12:31:59 -0500

    Version bump, refactoring to AbstractCommandMojo.

--- model/app.gen.maven/src/main/java/module-info.java
+++ model/app.gen.maven/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module org.nasdanika.html.model.app.gen.maven {
-	requires maven.plugin.api;
-	requires maven.plugin.annotations;
+	requires transitive org.nasdanika.maven;
 	requires transitive org.nasdanika.html.model.app.gen;
-	requires maven.project; 
 	
 	exports org.nasdanika.html.model.app.gen.maven;
 	
--- model/app.gen.maven/src/main/java/org/nasdanika/html/model/app/gen/maven/ActionSiteGeneratorMojo.java
+++ model/app.gen.maven/src/main/java/org/nasdanika/html/model/app/gen/maven/ActionSiteGeneratorMojo.java
@@ -1,22 +1,15 @@
 package org.nasdanika.html.model.app.gen.maven;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.CancellationException;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 import org.eclipse.emf.common.util.DiagnosticException;
 import org.eclipse.emf.common.util.URI;
 import org.nasdanika.common.Context;
@@ -24,155 +17,74 @@ import org.nasdanika.common.DiagramGenerator;
 import org.nasdanika.common.DiagramGeneratorImpl;
 import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.ProgressMonitor;
-import org.nasdanika.common.Status;
 import org.nasdanika.common.Util;
 import org.nasdanika.html.model.app.gen.ActionSiteGenerator;
+import org.nasdanika.maven.AbstractCommandMojo;
 
 /**
  * Generates action site.
  */
 @Mojo(name = "generate-action-site", defaultPhase = LifecyclePhase.SITE)
-public class ActionSiteGeneratorMojo extends AbstractMojo {
+public class ActionSiteGeneratorMojo extends AbstractCommandMojo {
 
+	/**
+	 * Directory to output generated site
+	 */
 	@Parameter(defaultValue = "target/action-site")	
 	private File outputDirectory;
 
+	/**
+	 * Working directory for storing intermediate files
+	 */
 	@Parameter(defaultValue = "target/action-site-work-dir")	
 	private File workDirectory;
 	
+	/**
+	 * If true the working directory is cleaned before generation
+	 */
 	@Parameter()	
 	private boolean cleanWorkDir;	
 	
+	/**
+	 * URI of the action from which to generate a site. Resolved relative to the project base directory
+	 */
 	@Parameter(required = true)	
 	private String action;
-	
+		
+	/**
+	 * URI of the page template. Resolved relative to the project base directory
+	 */
 	@Parameter(required = true)	
 	private String pageTemplate;
 	
+	/**
+	 * Site map domain/URL to output to sitemap.xml. 
+	 * Site map is not generated if this parameter is not set.
+	 */
 	@Parameter()	
 	private String siteMapDomain;
 	
+	/**
+	 * URL of the Drawio viewer script for rendering Drawio diagrams.
+	 * Set if you are hosting your own Drawio site.
+	 */
 	@Parameter()	
 	private String drawioViewer;	
 	
+	/**
+	 * Number of known/expected errors. E.g. some blank pages or broken links. Build fails if the number of actual errors reported is different from this parameter. 
+	 */
 	@Parameter(required = false)	
 	private int errors;
 	
-	@Parameter()	
-	private File progressOutput;	
-	
+	/**
+	 * Pluggable diagram generators.
+	 */
     @Parameter()
     private List<DiagramGenerator> diagramGenerators;
 	
-	
-//	@Parameter(name = "json-progress")	
-//	private boolean jsonProgress;	
-	
-	@Parameter(defaultValue = "${project}", required = true, readonly = true)
-	MavenProject project;	
-	
-	/**
-	 * Progress monitor reporting to a {@link PrintStream}, e.g. ``System.out``.
-	 * This monitor indents sub-tasks and worked messages. It can be thought of as a hierarchical logger.
-	 * @author Pavel
-	 *
-	 */
-	public class PrintStreamProgressMonitor implements ProgressMonitor {
-
-		private PrintStream out;
-		private boolean closeStream;
-		private String indent;
-		protected int indentIncrement = 2;
-		private boolean cancelled;
-		
-		/**
-		 * Constructs a progress monitor for a given print stream.
-		 * @param out
-		 * @param indent Indent in spaces for this monitor.
-		 * @param indentIncrement Increment to add to the indent of this monitor when constructing a sub-monitor.
-		 * @param closeStream if true the monitor closes the stream in its close() method.
-		 */
-		public PrintStreamProgressMonitor(PrintStream out, int indent, int indentIncrement, boolean closeStream) {
-			this.out = out;
-			StringBuilder indentBuilder = new StringBuilder();
-			for (int i = 0; i < indent; ++i) {
-				indentBuilder.append(' ');
-			}
-			this.indent = indentBuilder.toString();
-			this.closeStream = closeStream;
-		}
-		
-		/**
-		 * Constructs a progres monitor outputting to System.out with indent 0, indentIncrement 2 and not closing the stream.
-		 */
-		public PrintStreamProgressMonitor() {
-			this(System.out, 0, 2, false);
-		}
-		
-		@Override
-		public void close() {
-			if (closeStream) {
-				out.close();
-			}
-		}
-
-		@Override
-		public boolean isCancelled() {
-			return cancelled;
-		}
-
-		@Override
-		public ProgressMonitor split(String taskName, double size, Object... data) {
-			if (isCancelled()) {
-				throw new CancellationException();
-			}
-			out.println(indent+"  "+taskName+" ("+size+")");
-			if (data != null) {
-				for (Object d: data) {
-					out.println(formatDetail(d, indent + "    "));			
-				}
-			}
-			return new PrintStreamProgressMonitor(out, indent.length()+indentIncrement, indentIncrement, false) {
-				
-				@Override
-				public boolean isCancelled() {
-					return PrintStreamProgressMonitor.this.isCancelled();
-				}
-				
-			};
-		}
-		
-		/**
-		 * Formats detail element for output. This implementation concatenates the indent with the detail.
-		 * @param detail
-		 * @param indent
-		 * @return
-		 */
-		protected String formatDetail(Object detail, String indent) {
-			return indent + detail;
-		}
-
-		@Override
-		public void worked(Status status, double work, String progressMessage, Object... data) {
-			out.print(indent+"  ["+status+" "+work+"] "+progressMessage);
-			if (data.length > 0) { 
-				out.print(": "+Arrays.deepToString(data));
-			}
-			out.println();
-			if (status == Status.CANCEL) {
-				cancelled = true;
-			}
-		}
-
-		@Override
-		public ProgressMonitor setWorkRemaining(double size) {
-			// NOP
-			return this;
-		}
-
-	}	
-
-	public void execute() throws MojoExecutionException {
+	@Override
+	protected void execute(Context context, ProgressMonitor progressMonitor) {
 		
 		ActionSiteGenerator actionSiteGenerator = new ActionSiteGenerator() {
 			
@@ -195,25 +107,12 @@ public class ActionSiteGeneratorMojo extends AbstractMojo {
 						diagramGenerator = dg.compose(diagramGenerator);
 					}
 				}
-				return Context.singleton(DiagramGenerator.class, diagramGenerator).compose(super.createContext(progressMonitor));
+				return context.compose(Context.singleton(DiagramGenerator.class, diagramGenerator)).compose(super.createContext(progressMonitor));
 			}
 			
 			@Override
 			protected ProgressMonitor createProgressMonitor() {
-				if (progressOutput == null) {
-					return new LogProgressMonitor(getLog(), 0, 2);
-				}
-				
-//				if (jsonProgress) {
-//					// JSON output progress monitor
-//					
-//				}
-				
-				try {
-					return new PrintStreamProgressMonitor(new PrintStream(progressOutput), 0, 2, true);
-				} catch (FileNotFoundException e) {
-					throw new NasdanikaException(e);
-				}
+				return progressMonitor;
 			}
 			
 		};
@@ -242,10 +141,13 @@ public class ActionSiteGeneratorMojo extends AbstractMojo {
 				}
 			}
 			if (errorCount != this.errors) {
-				throw new MojoExecutionException("There are " + errorCount + " site errors");
+				String message = "There are " + errorCount + " site errors";
+				getLog().error(message);
+				throw new NasdanikaException(message);
 			}
 		} catch (IOException | DiagnosticException ex) {
-			throw new MojoExecutionException(ex);
+			throw new NasdanikaException(ex);
 		}
 	}
+
 }
