ab5c6d9e6d477f409d7771685e7c90523591ddfa
Pavel <Pavel.Vlasov@nasdanika.org>
2023-04-12 08:57:41 -0400

    Graph tests

--- model/app.graph/src/main/java/org/nasdanika/html/model/app/graph/Processor.java
+++ model/app.graph/src/main/java/org/nasdanika/html/model/app/graph/Processor.java
@@ -3,24 +3,15 @@ package org.nasdanika.html.model.app.graph;
 import java.util.Collection;
 
 import org.eclipse.emf.common.util.URI;
-import org.nasdanika.graph.emf.EReferenceConnection;
-import org.nasdanika.html.model.app.Action;
+import org.nasdanika.common.Supplier;
 import org.nasdanika.html.model.app.Label;
-import org.nasdanika.html.model.app.Link;
 
-public interface Processor {
+public interface Processor extends Supplier<Collection<Label>> {
 	
 	/**
 	 * Call for the root action, implementations shall propagate down the hierarchy and through action references.
 	 * @param base
 	 */
 	void resolveURI(URI base);
-
-	/**
-	 * Creates {@link Label}s and its subclasses - {@link Link}s and {@link Action}s for building an action model.
-	 * Processors may pass labels created by children, e.g. {@link EReferenceConnection} processors. 
-	 * @return
-	 */
-	Collection<Label> createLabels();
 
 }
