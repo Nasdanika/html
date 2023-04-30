27329acd4c8324a6c30c510c6819b5296cc6e2bb
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2023-04-30 11:32:07 -0400

    Icon, tooltip, label, and documentation from annotations

--- ecore.gen/src/main/java/org/nasdanika/html/ecore/gen/processors/EModelElementNodeProcessor.java
+++ ecore.gen/src/main/java/org/nasdanika/html/ecore/gen/processors/EModelElementNodeProcessor.java
@@ -4,13 +4,22 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.emf.ecore.EObject;
 import org.nasdanika.common.Context;
+import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.common.Util;
+import org.nasdanika.emf.EmfUtil;
+import org.nasdanika.emf.EmfUtil.EModelElementDocumentation;
+import org.nasdanika.emf.persistence.MarkerFactory;
+import org.nasdanika.exec.content.ContentFactory;
+import org.nasdanika.exec.content.Interpolator;
+import org.nasdanika.exec.content.Markdown;
+import org.nasdanika.exec.content.Text;
 import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.html.model.app.Action;
 import org.nasdanika.html.model.app.Label;
 import org.nasdanika.html.model.app.graph.LabelFactory;
 import org.nasdanika.html.model.app.graph.Registry;
 import org.nasdanika.html.model.app.graph.emf.EObjectNodeProcessor;
+import org.nasdanika.ncore.util.NcoreUtil;
 
 public class EModelElementNodeProcessor<T extends EModelElement> extends EObjectNodeProcessor<T> {
 
@@ -23,10 +32,60 @@ public class EModelElementNodeProcessor<T extends EModelElement> extends EObject
 	
 	@Override
 	protected void configureLabel(EObject eObject, Label label) {
-		if (Util.isBlank(label.getIcon()) && eObject instanceof EModelElement) {
-			label.setIcon("https://cdn.jsdelivr.net/gh/Nasdanika/html@master/ecore.gen/icons/" + ((EModelElement) eObject).eClass().getName() + ".gif");
+		if (eObject instanceof EModelElement) {
+			if (Util.isBlank(label.getIcon())) {
+				String defaultIcon = "https://cdn.jsdelivr.net/gh/Nasdanika/html@master/ecore.gen/icons/" + eObject.eClass().getName() + ".gif";
+				label.setIcon(NcoreUtil.getNasdanikaAnnotationDetail((EModelElement) eObject, "icon", defaultIcon));
+			}
+			if (Util.isBlank(label.getTooltip())) {
+				label.setTooltip(NcoreUtil.getNasdanikaAnnotationDetail((EModelElement) eObject, "description", null));
+			}			
 		}
 		super.configureLabel(eObject, label);
+	}
+	
+	@Override
+	protected Action newAction(EObject eObject) {
+		Action newAction = super.newAction(eObject);
+		if (eObject instanceof EModelElement && Util.isBlank(newAction.getText())) {
+			newAction.setText(NcoreUtil.getNasdanikaAnnotationDetail((EModelElement) eObject, "label", null));
+		}		
+		return newAction;
+	}
+	
+	@Override
+	protected Label createAction(ProgressMonitor progressMonitor) {
+		Label action = super.createAction(progressMonitor);
+		EModelElementDocumentation documentation = EmfUtil.getDocumentation((EModelElement) node.getTarget());
+		if (documentation != null) {
+			action.getContent().add(interpolatedMarkdown(context.interpolateToString(documentation.documentation()), documentation.location(), progressMonitor));			
+		}
+		return action;
+	}
+	
+	/**
+	 * @param markdown Markdown text
+	 * @return Spec for interpolating markdown and then converting to HTML. 
+	 */
+	protected Markdown interpolatedMarkdown(String markdown, URI location, ProgressMonitor progressMonitor) {
+		if (Util.isBlank(markdown)) {
+			return null;
+		}
+		Markdown ret = ContentFactory.eINSTANCE.createMarkdown();
+		Interpolator interpolator = ContentFactory.eINSTANCE.createInterpolator();
+		Text text = ContentFactory.eINSTANCE.createText();
+		text.setContent(markdown);
+		interpolator.setSource(text);
+		ret.setSource(interpolator);
+		ret.setStyle(true);
+		
+		// Creating a marker with EObject resource location for resource resolution in Markdown
+		if (location != null) {
+			org.nasdanika.ncore.Marker marker = context.get(MarkerFactory.class, MarkerFactory.INSTANCE).createMarker(location.toString(), progressMonitor);
+			ret.getMarkers().add(marker); 
+		}
+		
+		return ret;
 	}
 
 }
