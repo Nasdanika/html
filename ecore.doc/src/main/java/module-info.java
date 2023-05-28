22d2de424108c1767e62aca5ce1edac71f272602
Pavel <Pavel.Vlasov@nasdanika.org>
2023-05-28 17:07:27 -0400

    Using @EObjectNodeProcessor annotation

--- ecore.doc/src/main/java/module-info.java
+++ ecore.doc/src/main/java/module-info.java
@@ -4,5 +4,6 @@ module org.nasdanika.html.ecore.doc {
 	opens org.nasdanika.html.ecore.doc;
 	
 	requires transitive org.nasdanika.html.model.app;
+	requires transitive org.nasdanika.html.ecore.gen;
 		
 }
--- ecore.gen/src/main/java/org/nasdanika/html/ecore/gen/processors/EcoreNodeProcessorFactory.java
+++ ecore.gen/src/main/java/org/nasdanika/html/ecore/gen/processors/EcoreNodeProcessorFactory.java
@@ -1,25 +1,33 @@
 package org.nasdanika.html.ecore.gen.processors;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EDataType;
-import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EGenericType;
+import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EParameter;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.ETypeParameter;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.graph.processor.NodeProcessorConfig;
-import org.nasdanika.graph.processor.Processor;
 import org.nasdanika.graph.processor.emf.EObjectNodeProcessor;
 import org.nasdanika.graph.processor.emf.EObjectNodeProcessorReflectiveFactory;
 import org.nasdanika.html.model.app.Action;
 import org.nasdanika.html.model.app.graph.Registry;
 import org.nasdanika.html.model.app.graph.WidgetFactory;
+import org.nasdanika.html.model.app.graph.emf.EObjectReflectiveProcessorFactory;
 
 /**
  * Node processor factory to use with {@link EObjectReflectiveProcessorFactory}.
  * @author Pavel
  *
  */
-@EObjectNodeProcessor(type = EModelElement.class)
 public class EcoreNodeProcessorFactory extends EObjectNodeProcessorReflectiveFactory<Object, WidgetFactory, WidgetFactory, Registry<URI>> {
 	
 	private Context context;
@@ -40,57 +48,57 @@ public class EcoreNodeProcessorFactory extends EObjectNodeProcessorReflectiveFac
 		return new EPackageNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EClass'")
+	@EObjectNodeProcessor(type = EClass.class)
 	public EClassNodeProcessor createEClassNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EClassNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EDataType'")
+	@EObjectNodeProcessor(type = EDataType.class)
 	public EDataTypeNodeProcessor<EDataType> createEDataTypeNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EDataTypeNodeProcessor<EDataType>(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EEnum'")
+	@EObjectNodeProcessor(type = EEnum.class)
 	public EEnumNodeProcessor createEEnumNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EEnumNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EAnnotation'")
+	@EObjectNodeProcessor(type = EAnnotation.class)
 	public EAnnotationNodeProcessor createEAnnotationNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EAnnotationNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EEnumLiteral'")
+	@EObjectNodeProcessor(type = EEnumLiteral.class)
 	public EEnumLiteralNodeProcessor createEEnumLiteralNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EEnumLiteralNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EOperation'")
+	@EObjectNodeProcessor(type = EOperation.class)
 	public EOperationNodeProcessor createEOperationNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EOperationNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EParameter'")
+	@EObjectNodeProcessor(type = EParameter.class)
 	public EParameterNodeProcessor createEParameterNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EParameterNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EAttribute'")
+	@EObjectNodeProcessor(type = EAttribute.class)
 	public EAttributeNodeProcessor createEAttributeNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EAttributeNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EReference'")
+	@EObjectNodeProcessor(type = EReference.class)
 	public EReferenceNodeProcessor createEReferenceNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EReferenceNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'ETypeParameter'")
+	@EObjectNodeProcessor(type = ETypeParameter.class)
 	public ETypeParameterNodeProcessor createETypeParameterNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new ETypeParameterNodeProcessor(config, context, prototypeProvider);
 	}	
 	
-	@Processor("target.eClass().name == 'EGenericType'")
+	@EObjectNodeProcessor(type = EGenericType.class)
 	public EGenericTypeNodeProcessor createEGenericTypeNodeProcessor(NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config) {
 		return new EGenericTypeNodeProcessor(config, context, prototypeProvider);
 	}	
