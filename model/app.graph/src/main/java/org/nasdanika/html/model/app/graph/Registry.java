9567a3680769ef4ba1b28ce3dff42ff794801097
Pavel <Pavel.Vlasov@nasdanika.org>
2023-07-21 19:07:18 -0400

    Before deleting Registry - seems to be of little value

--- model/app.graph/src/main/java/org/nasdanika/html/model/app/graph/Registry.java
+++ model/app.graph/src/main/java/org/nasdanika/html/model/app/graph/Registry.java
@@ -3,6 +3,7 @@ package org.nasdanika.html.model.app.graph;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.nasdanika.graph.Element;
 import org.nasdanika.graph.processor.ProcessorInfo;
@@ -13,22 +14,72 @@ import org.nasdanika.graph.processor.ProcessorInfo;
  *
  * @param <I> Element identifier type
  */
-public interface Registry<I> {
+public abstract class Registry<I> {
 	
-	Map<Element, ProcessorInfo<Object,Registry<I>>> getProcessorInfoMap();
+	private Map<Element, ProcessorInfo<Object>> registry;
+
+	protected Registry(Map<Element, ProcessorInfo<Object>> registry) {
+		this.registry = registry;
+	}
+	
+	Map<Element, ProcessorInfo<Object>> getProcessorInfoMap();
 	
 	/**
 	 * Finds {@link WidgetFactory}ies by predicate
 	 * @param predicate
 	 * @return 
 	 */
-	Collection<WidgetFactory> select(Predicate<Element> predicate, NodeProcessor<I> base);
+	Collection<WidgetFactory> select(Predicate<Element> predicate, NodeProcessor base);
 	
 	/**
 	 * Finds a {@link WidgetFactory} by identifier
 	 * @param identifier
 	 * @return label record or null
 	 */
-	WidgetFactory find(I identifier, NodeProcessor<I> base);
+	WidgetFactory find(I identifier, NodeProcessor base);
+	
+	
+	
+	@Override
+	public Registry<I> createRegistry() {
+		return new Registry<I>() {
+
+			@Override
+			public Map<Element, ProcessorInfo<Object, Registry<I>>> getProcessorInfoMap() {
+				return registry;
+			}
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public Collection<WidgetFactory> select(Predicate<Element> predicate, NodeProcessor base) {
+				return registry
+						.entrySet()
+						.stream()
+						.filter(e -> predicate.test(e.getKey()))
+						.filter(e -> e.getValue().getProcessor() instanceof NodeProcessor)
+						.map(e -> ProcessorFactory.this.resolve((NodeProcessor<I>) e.getValue().getProcessor(), base))
+						.collect(Collectors.toList());
+			}
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public WidgetFactory find(I identifier, NodeProcessor base) {
+				return registry
+						.entrySet()
+						.stream()
+						.filter(e -> getIdentifiers(e.getKey()).contains(identifier))
+						.filter(e -> e.getValue().getProcessor() instanceof NodeProcessor)
+						.map(e -> ProcessorFactory.this.resolve((NodeProcessor<I>) e.getValue().getProcessor(), base))
+						.findFirst()
+						.orElse(null);
+			}
+			
+		};
+	}
+	
+	protected abstract WidgetFactory resolve(NodeProcessor<I> p, NodeProcessor<I> base);
+	
+	protected abstract Collection<I> getIdentifiers(Element element); 
+	
 	
 }
