7f4453ab5a7784d681454ff890ed362c0c92024b
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-01 07:09:40 -0500

    Mix-in

---
+++ model/app.graph/src/main/java/org/nasdanika/html/model/app/graph/emf/EObjectNodeProcessorMixIn.java
@@ -0,0 +1,9 @@
+package org.nasdanika.html.model.app.graph.emf;
+
+/**
+ * Base interface for {@link EObjectNodeProcessor} mix-ins to support multiple inheritance
+ * without duplication.
+ */
+public interface EObjectNodeProcessorMixIn {
+
+}
