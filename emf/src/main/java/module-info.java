edc6f46ac742205434e702505557269eba062e57
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-23 02:38:13 -0400

    Module dependency cleanup

--- emf/src/main/java/module-info.java
+++ emf/src/main/java/module-info.java
@@ -2,11 +2,9 @@ module org.nasdanika.html.emf {
 	
 	requires transitive org.nasdanika.html.model.app;
 	requires transitive org.nasdanika.emf;
-	requires transitive org.nasdanika.ncore;
 	requires org.apache.commons.lang3;
 	requires org.apache.commons.text;
 	requires org.apache.commons.codec;
-	requires org.nasdanika.html.model.bootstrap;
 	
 	exports org.nasdanika.html.emf;	
 	
--- model/app.gen/src/main/java/module-info.java
+++ model/app.gen/src/main/java/module-info.java
@@ -1,18 +1,14 @@
 module org.nasdanika.html.model.app.gen {
 		
-	requires transitive org.nasdanika.ncore;
-	requires transitive org.nasdanika.html.model.app;
 	requires transitive org.nasdanika.html.model.bootstrap.gen;
 	requires transitive org.nasdanika.html.jstree;
 	requires transitive org.nasdanika.html.emf;
-	requires transitive org.nasdanika.drawio;
 	requires transitive sitemapgen4j;
 	requires org.apache.commons.codec;
 	requires org.apache.commons.text;
 	requires transitive org.jsoup;
 	requires org.eclipse.emf.ecore.xmi;
 	requires org.eclipse.emf.ecore;
-	requires org.nasdanika.persistence;
 	
 	exports org.nasdanika.html.model.app.gen;
 	opens org.nasdanika.html.model.app.gen;
