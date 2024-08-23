695406126636555611d6c18c24b6570ff126e0f9
Pavel <Pavel.Vlasov@nasdanika.org>
2024-08-23 08:55:27 -0400

    Initial drawio action generator command

--- model/app.gen.cli/src/main/java/org/nasdanika/html/model/app/gen/cli/DrawioActionGeneratorCommand.java
+++ model/app.gen.cli/src/main/java/org/nasdanika/html/model/app/gen/cli/DrawioActionGeneratorCommand.java
@@ -1,24 +1,17 @@
 package org.nasdanika.html.model.app.gen.cli;
 
-import java.io.File;
 import java.util.Collection;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.function.Consumer;
 
 import org.nasdanika.cli.CommandGroup;
 import org.nasdanika.cli.ModuleVersionProvider;
 import org.nasdanika.cli.ParentCommands;
 import org.nasdanika.common.Diagnostic;
-import org.nasdanika.common.PrintStreamProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.common.Supplier;
 import org.nasdanika.drawio.Document;
-import org.nasdanika.html.bootstrap.Theme;
 import org.nasdanika.html.model.app.Action;
-import org.nasdanika.html.model.app.AppFactory;
 import org.nasdanika.html.model.app.Label;
-import org.nasdanika.html.model.app.gen.ActionSiteGenerator;
 import org.nasdanika.html.model.app.graph.drawio.DrawioActionGenerator;
 import org.nasdanika.html.model.app.util.ActionSupplier;
 
@@ -43,34 +36,7 @@ public class DrawioActionGeneratorCommand extends CommandGroup implements Action
 		Supplier<Collection<Label>> labelSupplier = actionGenerator.createLabelsSupplier(document, progressMonitor);
 		Consumer<Diagnostic> diagnosticConsumer = createDiagnosticConsumer();
 		Collection<Label> labels = labelSupplier.call(progressMonitor, diagnosticConsumer);
-		
-		
-		
-		Action rootAction = AppFactory.eINSTANCE.createAction();
-		rootAction.setText("Nasdanika");		
-		rootAction.getChildren().addAll(labels);
-		
-		String siteMapDomain = "https://nasdanika.org/demos/" + diagramName;		
-		ActionSiteGenerator actionSiteGenerator = new ActionSiteGenerator();		
-		
-		Map<String, Collection<String>> errors = actionSiteGenerator.generate(
-				rootAction, // URI.appendFragment("/"), 
-				Theme.Cerulean.pageTemplateCdnURI, 
-				siteMapDomain, 
-				new File("target/drawio-action-site/" + diagramName), 
-				new File("target/doc-site-work-dir/" + diagramName), 
-				false);
-				
-		int errorCount = 0;
-		for (Entry<String, Collection<String>> ee: errors.entrySet()) {
-			System.err.println(ee.getKey());
-			for (String error: ee.getValue()) {
-				System.err.println("\t" + error);
-				++errorCount;
-			}
-		}
-		
-		System.out.println(errorCount);
+		return reduce(labels);
 	}
 	
 	protected Action reduce(Collection<Label> labels) {
