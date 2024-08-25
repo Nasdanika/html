0a613364355fb2b1fdb7e7ea9fbf6edd8ab2ba3f
Pavel <Pavel.Vlasov@nasdanika.org>
2024-08-25 10:03:05 -0400

    Cleanup

--- model/app.gen.cli/src/main/java/org/nasdanika/html/model/app/gen/cli/AbstractHtmlAppGeneratorCommand.java
+++ model/app.gen.cli/src/main/java/org/nasdanika/html/model/app/gen/cli/AbstractHtmlAppGeneratorCommand.java
@@ -9,7 +9,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.nasdanika.cli.CommandGroup;
 import org.nasdanika.cli.ResourceSetMixIn;
-import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.common.Util;
 import org.nasdanika.drawio.Document;
@@ -82,35 +81,31 @@ public abstract class AbstractHtmlAppGeneratorCommand extends CommandGroup imple
 			throw new UnsupportedOperationException("Multiple labels, use root action option");
 		}
 		
-		try {
-			URI rootURI = isFile ? URI.createFileURI(new File(this.rootAction).getAbsolutePath()) : URI.createURI(this.rootAction).resolve(URI.createFileURI(new File(".").getAbsolutePath()));
-			if (!rootURI.hasFragment()) {
-				rootURI = rootURI.appendFragment("/");
-			}
-			ResourceSet rSet = resourceSetMixIn.createResourceSet(progressMonitor);		
-			Label root = (Label) rSet.getEObject(rootURI, true);
-			EList<EObject> rootChildren = root.getChildren();
-			if (rootChildren.isEmpty()) {
-				rootChildren.addAll(labels);
-			} else if (addToRoot) {
-				rootChildren.addAll(insertionIndex, labels);				
-			} else {
-				EObject principal = rootChildren.get(0);
-				if (principal instanceof Label) {
-					EList<EObject> principalChildren = ((Label) principal).getChildren();
-					if (principalChildren.isEmpty()) {
-						principalChildren.addAll(labels);
-					} else {
-						principalChildren.addAll(insertionIndex, labels);						
-					}
-				} else {
-					rootChildren.addAll(insertionIndex, labels);					
-				}
-			}
-			return root;
-		} catch (Exception e) {
-			throw new NasdanikaException(e); 
+		URI rootURI = isFile ? URI.createFileURI(new File(this.rootAction).getAbsolutePath()) : URI.createURI(this.rootAction).resolve(URI.createFileURI(new File(".").getAbsolutePath()).appendSegment(""));
+		if (!rootURI.hasFragment()) {
+			rootURI = rootURI.appendFragment("/");
 		}
+		ResourceSet rSet = resourceSetMixIn.createResourceSet(progressMonitor);		
+		Label root = (Label) rSet.getEObject(rootURI, true);
+		EList<EObject> rootChildren = root.getChildren();
+		if (rootChildren.isEmpty()) {
+			rootChildren.addAll(labels);
+		} else if (addToRoot) {
+			rootChildren.addAll(insertionIndex, labels);				
+		} else {
+			EObject principal = rootChildren.get(0);
+			if (principal instanceof Label) {
+				EList<EObject> principalChildren = ((Label) principal).getChildren();
+				if (principalChildren.isEmpty()) {
+					principalChildren.addAll(labels);
+				} else {
+					principalChildren.addAll(insertionIndex, labels);						
+				}
+			} else {
+				rootChildren.addAll(insertionIndex, labels);					
+			}
+		}
+		return root;
 	}
 
 }
